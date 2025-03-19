a7a4da9f1589a1f2fadda7475839d16a1c5b68c7
tajshuvoo <tajshuvo75@gmail.com>
2025-03-19 17:15:57 +0600

    all user fetcher

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { loginUser, registerUser, logoutUser } from '../controllers/authController.js';
+import { loginUser, registerUser, logoutUser ,getAllUserEmails} from '../controllers/authController.js';
 import { protect,admin } from '../middleware/authmiddleware.js';
+
 const router = express.Router();
 
 router.post('/login', loginUser); // Route for login
 router.post('/signup', registerUser); // Route for signup
 router.post('/logout',protect, logoutUser); // Route for logout
-//bug solved
+router.get('/users', protect, getAllUserEmails); // Route to fetch all users
 
 export default router;
