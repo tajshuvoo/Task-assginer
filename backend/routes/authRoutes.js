9e7d20211ed4e1af7d997ba4101161183186f601
tajshuvoo <tajshuvo75@gmail.com>
2025-03-19 15:51:09 +0600

    log out api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/authController.js'; // Import registerUser
+import { loginUser, registerUser, logoutUser } from '../controllers/authController.js'; // Import logoutUser
 
 const router = express.Router();
 
 router.post('/login', loginUser); // Route for login
 router.post('/signup', registerUser); // Route for signup
+router.post('/logout', logoutUser); // Route for logout
 
 export default router;
